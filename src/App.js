7a10dab4aa636853737e1747101ffd83dacd8517
IQBAL AHMAD <116335338+Iqbal-Farooq@users.noreply.github.com>
2023-01-22 13:57:28 +0530

    Add files via upload
    Logout the USER

--- src/App.js
+++ src/App.js
@@ -6,23 +6,38 @@ import Profile from "./Expenses/Profile";
 
 import { AuthContext } from "./AUth/AuthContext";
 import { useContext,} from "react";
-import { Route,} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { Route,Redirect} from "react-router-dom";
+
+import { Navbar, NavLink, } from "react-bootstrap";
 
 
 
 function App() {
   const ctx=useContext(AuthContext)
+  const history=useHistory();
+
+  const Logut=()=>{
+    ctx.Logout();
+    history.replace("./Singup/Signup")
+
+  }
  
   return ( 
+    <>
+    <Navbar> {ctx.isLogin && <NavLink  to='#'><button onClick={Logut}> Logout</button> </NavLink>}</Navbar>
     <switch>
  
                             
                       {ctx.isLogin && <Route path="/Expenses/ExpenseItem"><ExpansesItem /></Route>}
                      {!ctx.isLogin && <SignUp />} 
                      <Route path="/Expenses/Profile" ><Profile /></Route>
+                     {/* <Route path="/Singup/Logout">{ ctx.isLogin &&  <LogOut></LogOut>} 
+                     {!ctx.isLogin && <Redirect to="/Singup/Logout" />}
+                     </Route> */}
   
     </switch>
- 
+ </>
   )
 }
 
