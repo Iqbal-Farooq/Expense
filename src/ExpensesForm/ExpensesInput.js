34ba0f67ae7228cbc224a345f7b3850260068525
IQBAL AHMAD <116335338+Iqbal-Farooq@users.noreply.github.com>
2023-01-24 18:57:37 +0530

    Add files via upload
    Deleting and editing expense

--- src/ExpensesForm/ExpensesInput.js
+++ src/ExpensesForm/ExpensesInput.js
@@ -1,61 +1,100 @@
-import { useState,useEffect } from 'react';
+import { useState,useEffect, useRef } from 'react';
 
 import{Row,Col,Form,Card,Button,Container} from 'react-bootstrap'
 import axios from 'axios';
 
 
 const ExpenseForm = (props) => {
-const [enteredExpense,setEnteredExpense]=  useState('')
-const [enteredDetails,setEnteredDetails] = useState('')
-const [enteredCategory,setEnteredCategory] = useState('')
+  // const [length,Setlength]=useState(false);
 const [enteredData,setEnteredData]=useState([]);
 
-useEffect(()=>{
+const  ExpenseAmount=useRef();
+const   ExpenseDisc=useRef();
+const  ExpenseCat=useRef();
+// if(enteredData.length>0){
+//   Setlength(true)
+//   console.log(enteredData.length)
+// }
+
+
   async function GetDate(){
     let res=await axios.get('https://expense-tracker-e38a4-default-rtdb.firebaseio.com/expenseItems.json');
-    const data = await Object.values(res.data);
-    console.log(res);
-    // setEnteredData(data);
+    
+    const data= await res.data;
+
+    console.log("INSIDE useuEFFEct",data);
+   
+     setEnteredData(data);
+      
   
   }
+
+useEffect(()=>{
+
   GetDate()
 },[])
 
 
-
-const expenseHandler  =(event) =>{
- 
-
-
-  setEnteredExpense(event.target.value)  
-}
-const detailsHandler=(event)=>{
-  
-
-    setEnteredDetails(event.target.value)  
-}
-const categoryHandler =(event)=>{
-
-    setEnteredCategory(event.target.value)  
-}
-async function SubmitHandler (event){
-event.preventDefault()
+async function SubmitHandler (event ){
+  event.preventDefault();
+//   let index=enteredData.findIndex(item=>Object.keys(item)===expenseId) ;
+//   console.log(index);
+  let enteredamount=ExpenseAmount.current.value;
+ let entereddisc=ExpenseDisc.current.value;
+ let enteredcate=ExpenseCat.current.value;
 
 const obj = {
-    enteredExpense,
-    enteredDetails,
-    enteredCategory
+  amount:enteredamount,
+  disc:entereddisc,
+  cate:enteredcate
+ 
+
 }
+
+  // if(index>=0){
+  //   const res=await axios.put(`https://expense-tracker-e38a4-default-rtdb.firebaseio.com/expenseItems/${index}.json`,obj);
+  //   let data=await res.data;
+
+  // }else{
+    
 let res=await axios.post('https://expense-tracker-e38a4-default-rtdb.firebaseio.com/expenseItems.json',obj);
 let data=await res.data;
-console.log(data);
+console.log("DATA FINDING",data)
 
-setEnteredData((prevexpense)=>{
-  return [...prevexpense,obj]
-})
+  // }
 
+ ;
+
+
+// setEnteredData(prev=>{return  {...prev,obj}})
+
+  GetDate();
+
+
+  
 
 }
+
+async function onExpenseDeleteClickHandler(id){
+
+  const res=await axios.delete(`https://expense-tracker-e38a4-default-rtdb.firebaseio.com/expenseItems/${id}.json`);
+  const data= await res.data;
+  console.log("SuccessFully");
+   GetDate();
+
+}
+ const onEditExpenseClickHandler = (expenseId) => {
+
+    ExpenseAmount.current.value = enteredData[expenseId].amount;
+    ExpenseDisc.current.value = enteredData[expenseId].disc;
+    ExpenseCat.current.value = enteredData[expenseId].cate;
+
+  //  SubmitHandler(expenseId);
+
+    onExpenseDeleteClickHandler(expenseId);
+    GetDate();
+  }
+
 return (
     <>
      <Row style={{margin:"2% 0  0  3%"}}>
@@ -71,21 +110,21 @@ return (
                    
 
                         <Form.Group   className="mb-1">
-                            <Form.Control size="lg" type="number" placeholder="amount" name="amount" onChange={expenseHandler}  ></Form.Control>
+                            <Form.Control size="lg" type="number" placeholder="amount" name="amount" ref={ExpenseAmount}  ></Form.Control>
                             </Form.Group>
                          <Form.Group className="mb-2">
-                            <Form.Control size="lg" type="text" placeholder="discription" name="discription"  onChange={detailsHandler}></Form.Control>
+                            <Form.Control size="lg" type="text" placeholder="discription" name="discription"  ref={ExpenseDisc}></Form.Control>
                              </Form.Group>
 
                              
                          <Form.Group className="mb-1">
                           <Form.Label style={{fontWeight:"bold",marginRight:"30px"}}>Category</Form.Label>
 
-                            <select onChange={categoryHandler} value={enteredCategory} style={{width:"80%"}} >
+                            <select ref={ExpenseCat} style={{width:"80%"}} >
                                 <option>Food</option>
-                                <option>Petrol</option>
+                                <option>Sports</option>
                                 <option>Salary</option>
-                                <option>Travlling</option>
+                                <option>Travelling</option>
                                 <option>Study</option>
                                 <option>House Keeping</option>
                             </select>
@@ -106,14 +145,24 @@ return (
          </Col>
 
         </Row>
+        <div style={{marginTop:"30px"}}>  
 
-        {enteredData.map((item)=>{return<li>
-        <span>{item.enteredExpense}</span>
-         <span>{item.enteredDetails}</span> 
-          <span> {item.enteredCategory}</span></li>})}
-      
-      
-
+       { Object.keys(enteredData).map((key)=>{ 
+        return  <div style={{marginTop:"15px"} }><li key={key}>
+         <span style={{margin:"1px 3px 1px 1px",}}>{enteredData[key].amount}</span>
+            <span style={{margin:"1px 3px 1px 10px"}}>{enteredData[key].disc}</span> 
+            <span style={{margin:"1px 10px 1px 10px"}}>{enteredData[key].cate}</span>
+             <span style={{margin:"6px 15px 1px 1px"}}> <Button size='sm' variant='success' onClick={()=>{onEditExpenseClickHandler(key)}}>Edit</Button></span>
+            <span style={{margin:"6px 10px 1px 1px"}}><Button size='sm' variant='danger' onClick={()=>{onExpenseDeleteClickHandler(key)}}>Delete</Button> </span>
+             </li>
+            </div>
+           
+         
+            })
+        } 
+         {console.log(Object.values(enteredData))}
+ 
+         </div>
     </>
   )
 };
